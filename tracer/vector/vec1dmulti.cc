71f045518c440e205ac5afac1653b9812b94cb03
g-ram <ggobieski@yahoo.com>
2019-02-19 18:56:01 -0500

    fixed timing model for vec1dmulti

--- tracer/vector/vec1dmulti.cc
+++ tracer/vector/vec1dmulti.cc
@@ -5,7 +5,7 @@
 #include "pending_event.h"
 
 vec1dmulti_t::vec1dmulti_t(std::string _name, io::json _config, 
-	event_heap_t *_events) : vcu_t(_name, _config, _events) {
+	event_heap_t *_events) : vcu_t(_name, _config, _events), retire_insn(nullptr) {
 	JSON_CHECK(int, config["window_size"], window_size);
 	assert_msg(window_size > 0, "Window size must be greater than zero");
 	JSON_CHECK(int, config["rf_ports"], rf_ports);
@@ -20,7 +20,7 @@ io::json vec1dmulti_t::to_json() const {
 void vec1dmulti_t::reset(reset_level_t level) {
 	vcu_t::reset(level);
 	idx = 0;
-	start = false;
+	pe_state = READ;
 	active_window_size = 0;
 	active_insn_offset = 0;
 	active_reg_reads = 0;
@@ -35,10 +35,12 @@ void vec1dmulti_t::process(vector_exec_event_t *event) {
 	if(promote_pending(event, [&](){
 		return !(active_window_size < window_size);
 	}) != nullptr) return;
+	
 	empty = false;
 	event->data->idx = idx;
 	idx++;
 	active_window_size++;
+
 	// Add to table here
 	for(auto it : event->data->ws.input.vregs) {
 		uint8_t reg = strip_killed(it);
@@ -46,169 +48,199 @@ void vec1dmulti_t::process(vector_exec_event_t *event) {
 		if(!found) read_set.insert(reg);
 		else if(check_killed(it) && found) write_set.erase(reg);
 	}
+
 	for(auto it : event->data->ws.output.vregs) {
 		uint8_t reg = strip_killed(it);
 		write_set.insert(reg);
 	}
+
 	if(active_window_size == window_size) {
 		vcu_t::set_core_stage("exec", true);
 		events->push_back(new vector_start_event_t(this, false, clock.get()));
-		auto pending_event = new pending_event_t(this, 
-		new pe_exec_event_t(this, event->data, clock.get()), clock.get() + 1);
-		pending_event->add_dep([&]() { 
-			return active_reg_reads >= read_set.size();
-		});
-		register_pending(pending_event);
-		events->push_back(pending_event);
-		return;
 	}
+
 	auto pending_event = new pending_event_t(this, 
 		new pe_exec_event_t(this, event->data, clock.get()), clock.get() + 1);
 	pending_event->add_dep<vector_start_event_t *>([](vector_start_event_t *e) {
 		return true;
 	});
-	if(start){
-		pending_event->add_dep([&]() { 
-			return active_reg_reads >= read_set.size();
-		});
+	pending_event->add_dep([&](){ return pe_state == EXEC; });
+	register_pending(pending_event);
+	events->push_back(pending_event);
+
+	if(active_window_size == 1) {
+		auto pending_read_event = new pending_event_t(this, 
+			new pe_read_event_t(this, &read_set, clock.get()), clock.get() + 1);
+		pending_read_event->add_dep<vector_start_event_t *>(
+			[](vector_start_event_t *e) { return true; });
+		register_pending(pending_read_event);
+		events->push_back(pending_read_event);
+
+		auto pending_write_event = new pending_event_t(this, 
+			new pe_write_event_t(this, &write_set, clock.get()), clock.get() + 1);
+		pending_write_event->add_dep<vector_start_event_t *>(
+			[](vector_start_event_t *e) { return true; });
+		pending_write_event->add_dep([&](){ return pe_state == WRITE; });
+		register_pending(pending_write_event);
+		events->push_back(pending_write_event);
 	}
-	start = true;
+	retire_insn = event->data;
+}
+
+void vec1dmulti_t::process(pe_read_event_t *event) {
+	TIME_VIOLATION_CHECK
+	uint16_t work = read_set.size() - active_reg_reads;
+	if(work > rf_ports) work = rf_ports;
+	pending_event_t *pending_event;
+
+	if(active_reg_writes + work < read_set.size()) {
+		 pending_event = new pending_event_t(this, 
+			new pe_read_event_t(this, event->data), clock.get() + 1);
+		pending_event->add_fini([&, work](){ active_reg_reads += work; });
+	} else if(progress + lanes < vl) {
+		pe_state = EXEC;
+		active_reg_reads = 0;
+		pending_event = new pending_event_t(this, 
+			new pe_read_event_t(this, event->data), clock.get() + 1);
+		pending_event->add_dep([&](){ return pe_state == READ; });
+	} else {
+		pe_state = EXEC;
+		active_reg_reads = 0;
+		pending_event = new pending_event_t(this, nullptr, clock.get() + 1);
+	}
+
+	auto it = std::next(read_set.begin(), active_reg_reads);
+	auto end = std::next(read_set.begin(), active_reg_reads + work);
+	while(it != end) {
+		auto reg = *it;
+		events->push_back(new vector_reg_read_event_t(
+			this, {.reg=reg, .idx=0}, clock.get()));
+		pending_event->add_dep<vector_reg_read_event_t *>(
+			[reg](vector_reg_read_event_t *e){
+				return e->data.reg == reg;
+		});
+		++it;
+	}
 	register_pending(pending_event);
 	events->push_back(pending_event);
 }
 
 void vec1dmulti_t::process(pe_exec_event_t *event) {
 	TIME_VIOLATION_CHECK
-	if(active_reg_reads < read_set.size()) { // Issue register reads
-		std::cerr << "reading registers" << std::endl;
-		uint16_t work = read_set.size();
-		if(work > rf_ports) work = rf_ports;
-		auto pending_event = new pending_event_t(this, 
+	// Promote to pending
+	uint16_t insn_idx = event->data->idx;
+	if(promote_pending(event, [&, insn_idx]() {
+		return !(active_insn_offset == insn_idx);
+	}) != nullptr) return;
+
+	uint16_t work = vl - progress;
+	if(work > lanes) work = lanes;
+
+	pending_event_t *pending_event;
+	if(progress + lanes < vl){
+		pending_event = new pending_event_t(this, 
 			new pe_exec_event_t(this, event->data), clock.get() + 1);
-		pending_event->add_fini([&, work](){
-			active_reg_reads += work;
+		pending_event->add_dep([&, cur_progess=progress]() { 
+			return pe_state == EXEC && cur_progess < progress; 
 		});
-		auto it = std::next(read_set.begin(), active_reg_reads);
-		auto end = std::next(read_set.begin(), active_reg_reads + work);
-		while(it != end) {
-			auto reg = *it;
-			events->push_back(new vector_reg_read_event_t(
-				this, {.reg=reg, .idx=0}, clock.get()));
-			pending_event->add_dep<vector_reg_read_event_t *>(
-				[reg](vector_reg_read_event_t *e){
-					return e->data.reg == reg;
-			});
-			++it;
-		}
-		register_pending(pending_event);
-		events->push_back(pending_event);
-	} else if(progress < vl) { // Compute
-		// Promote to pending
-		std::cerr << "computing" << std::endl;
-		uint16_t insn_idx = event->data->idx;
-		if(promote_pending(event, [&, insn_idx]() {
-			return !(active_insn_offset == insn_idx && outstanding == 0);
-		}) != nullptr) return;
-		uint16_t work = vl - progress;
-		if(work > lanes) work = lanes;
-		pending_event_t *pending_event;
-
-		if(progress + work < vl || insn_idx == 0){
-			pending_event = new pending_event_t(this, 
-				new pe_exec_event_t(this, event->data), clock.get() + 1);
-		} else {
-			pending_event = new pending_event_t(this, nullptr, clock.get() + 1);
-		}
-
-		pending_event->add_fini([&, insn_idx, work](){
-			outstanding = 0;
-			if(insn_idx == active_window_size - 1) progress += work;
-			active_insn_offset++;
-			if(active_insn_offset == active_window_size) 
-				active_insn_offset = 0;
-		});
-
-		// Input locations
-		if(progress < event->data->ws.input.locs.size()) {
-			auto it = std::next(event->data->ws.input.locs.begin(), progress);
-			auto end = std::next(
-				event->data->ws.input.locs.begin(), progress + work);
-			while(it != end) {
-				for(auto child : children.raw<ram_handler_t *>()) {
-					auto loc = *it;
-					events->push_back(
-						new mem_read_event_t(child.second, loc, clock.get()));
-					pending_event->add_dep<mem_retire_event_t *>(
-						[loc](mem_retire_event_t *e){
-						return e->data.addr == loc;
-					});
-					outstanding = 1;
-				}
-				++it;
-			}
-		}
-
-		// Output locations
-		if(progress < event->data->ws.output.locs.size()) {
-			auto it = std::next(event->data->ws.output.locs.begin(), progress);
-			auto end = std::next(
-				event->data->ws.output.locs.begin(), progress + work);
-			while(it != end) {
-				for(auto child : children.raw<ram_handler_t *>()) {
-					auto loc = *it;
-					events->push_back(
-						new mem_read_event_t(child.second, loc, clock.get()));
-					pending_event->add_dep<mem_retire_event_t *>(
-						[loc](mem_retire_event_t *e){
-						return e->data.addr == loc;
-					});
-					outstanding = 1;
-				}
-				++it;
-			}
-		}
-
-		register_pending(pending_event);
-		events->push_back(pending_event);
-	} else { // Issue register writes
-		std::cerr << "writing registers" << std::endl;
-		uint16_t work = write_set.size() - active_reg_writes;
-		pending_event_t *pending_event;
-		if(work > rf_ports) {
-			work = rf_ports;
-			pending_event = new pending_event_t(this, 
-				new pe_exec_event_t(this, event->data), clock.get() + 1);
-			pending_event->add_fini([&, work](){
-				active_reg_writes += work;
-			});
-		} else {
-			pending_event = new pending_event_t(this, 
-				new pe_ready_event_t(this, event->data), clock.get() + 1);
-			pending_event->add_fini([&](){ 
-				vcu_t::set_core_stage("exec", false);
-			});
-		}
-		auto it = std::next(write_set.begin(), active_reg_writes);
-		auto end = std::next(write_set.begin(), active_reg_writes + work);
-		while(it != end) {
-			auto reg = *it;
-			events->push_back(new vector_reg_write_event_t(
-				this, {.reg=reg, .idx=0}, clock.get()));
-			pending_event->add_dep<vector_reg_write_event_t *>(
-				[reg](vector_reg_write_event_t *e){
-					return e->data.reg == reg;
-			});
-			++it;
-		}
-		register_pending(pending_event);
-		events->push_back(pending_event);
+	} else {
+		pending_event = new pending_event_t(this, nullptr, clock.get() + 1);
 	}
+
+	// THIS IS SORT OF WRONG
+	active_insn_offset++;
+	if(insn_idx == active_window_size - 1) {
+		pe_state = WRITE;
+		active_insn_offset = 0;
+	}
+
+	// Input locations
+	if(progress < event->data->ws.input.locs.size()) {
+		auto it = std::next(event->data->ws.input.locs.begin(), progress);
+		auto end = std::next(
+			event->data->ws.input.locs.begin(), progress + work);
+		while(it != end) {
+			for(auto child : children.raw<ram_handler_t *>()) {
+				auto loc = *it;
+				events->push_back(
+					new mem_read_event_t(child.second, loc, clock.get()));
+				pending_event->add_dep<mem_retire_event_t *>(
+					[loc](mem_retire_event_t *e){
+					return e->data.addr == loc;
+				});
+			}
+			++it;
+		}
+	}
+
+	// Output locations
+	if(progress < event->data->ws.output.locs.size()) {
+		auto it = std::next(event->data->ws.output.locs.begin(), progress);
+		auto end = std::next(
+			event->data->ws.output.locs.begin(), progress + work);
+		while(it != end) {
+			for(auto child : children.raw<ram_handler_t *>()) {
+				auto loc = *it;
+				events->push_back(
+					new mem_read_event_t(child.second, loc, clock.get()));
+				pending_event->add_dep<mem_retire_event_t *>(
+					[loc](mem_retire_event_t *e){
+					return e->data.addr == loc;
+				});
+			}
+			++it;
+		}
+	}
+
+	register_pending(pending_event);
+	events->push_back(pending_event);
+}
+
+void vec1dmulti_t::process(pe_write_event_t *event) {
+	TIME_VIOLATION_CHECK
+	uint16_t work = write_set.size() - active_reg_writes;
+	if(work > rf_ports) work = rf_ports;
+
+	pending_event_t *pending_event;
+	if(active_reg_writes < write_set.size()) {
+		pending_event = new pending_event_t(this, 
+			new pe_write_event_t(this, event->data), clock.get() + 1);
+		pending_event->add_fini([&, work](){ active_reg_writes += work; });
+	} else if(progress + lanes < vl) {
+		pe_state = READ;
+		active_reg_writes = 0; 
+		progress += lanes;
+		pending_event = new pending_event_t(this, 
+			new pe_write_event_t(this, event->data), clock.get() + 1);
+		pending_event->add_dep([&](){ return pe_state == WRITE; });
+	} else {
+		pending_event = new pending_event_t(this, 
+			new pe_ready_event_t(this, retire_insn), clock.get() + 1);
+		pending_event->add_fini([&](){ 
+			vcu_t::set_core_stage("exec", false);
+		});
+	}
+
+	auto it = std::next(write_set.begin(), active_reg_writes);
+	auto end = std::next(write_set.begin(), active_reg_writes + work);
+	while(it != end) {
+		auto reg = *it;
+		events->push_back(new vector_reg_write_event_t(
+			this, {.reg=reg, .idx=0}, clock.get()));
+		pending_event->add_dep<vector_reg_write_event_t *>(
+			[reg](vector_reg_write_event_t *e){
+				return e->data.reg == reg;
+		});
+		++it;
+	}
+	register_pending(pending_event);
+	events->push_back(pending_event);
 }
 
 void vec1dmulti_t::process(pe_ready_event_t *event) {
 	TIME_VIOLATION_CHECK
 	idx = 0;
-	start = false;
+	pe_state = READ;
 	active_window_size = 0;
 	active_insn_offset = 0;
 	active_reg_reads = 0;
@@ -219,8 +251,8 @@ void vec1dmulti_t::process(pe_ready_event_t *event) {
 	empty = true;
 	for(auto parent : parents.raw<vector_signal_handler_t *>()) {
 		events->push_back(
-			new vector_ready_event_t(parent.second, event->data, clock.get()));
+			new vector_ready_event_t(parent.second, retire_insn, clock.get()));
 		events->push_back(
-			new vector_retire_event_t(parent.second, event->data, clock.get()));
+			new vector_retire_event_t(parent.second, retire_insn, clock.get()));
 	}
 }
--- tracer/vector/vec1dmulti.h
+++ tracer/vector/vec1dmulti.h
@@ -3,31 +3,71 @@
 
 #include <set>
 #include <vector>
+#include <sstream>
 
 #include "vcu.h"
 
 class vec1dmulti_t: public vcu_t {
 public:
+	struct pe_read_event_t: public event_t<vec1dmulti_t, std::set<uint8_t> *> {
+		using event_t<vec1dmulti_t, std::set<uint8_t> *>::event_t;
+		std::string to_string() {
+			std::ostringstream os;
+			os << "pe_read_event_t (" << this->cycle << ", ";
+			auto it = this->data->begin();
+			auto end = std::prev(this->data->end(), 1);
+			while(it != this->data->end()) {
+				os << static_cast<uint16_t>(*it) << ", ";
+				it++;
+			}
+			os << static_cast<uint16_t>(*it) << ")";
+			return os.str();
+		}
+		std::string get_name() { return "pe_read_event_t"; }
+		HANDLER;
+	};
+	struct pe_write_event_t: public event_t<vec1dmulti_t, std::set<uint8_t> *> {
+		using event_t<vec1dmulti_t, std::set<uint8_t> *>::event_t;
+		std::string to_string() {
+			std::ostringstream os;
+			os << "pe_write_event_t (" << this->cycle << ", ";
+			auto it = this->data->begin();
+			auto end = std::prev(this->data->end(), 1);
+			while(it != this->data->end()) {
+				os << static_cast<uint16_t>(*it) << ", ";
+				it++;
+			}
+			os << static_cast<uint16_t>(*it) << ")";
+			return os.str();
+		}
+		std::string get_name() { return "pe_write_event_t"; }
+		HANDLER;
+	};
+public:
 	vec1dmulti_t(std::string _name, io::json _config, event_heap_t *_events);
 	io::json to_json() const;
 	void reset(reset_level_t level);
 	void process(vector_exec_event_t *event);
+	void process(pe_read_event_t *event);
 	void process(pe_exec_event_t *event);
+	void process(pe_write_event_t *event);
 	void process(pe_ready_event_t *event);
 protected:
 	uint32_t window_size = 1;
 	uint32_t rf_ports = 1;
 
 	uint16_t idx = 0;
-	bool start = false;
 	uint16_t active_window_size = 0;
 	uint16_t active_insn_offset = 0;
 	uint16_t active_reg_reads = 0;
 	uint16_t active_reg_writes = 0;
 	uint16_t progress = 0;
+	enum pe_state_t {READ, WRITE, EXEC};
+	pe_state_t pe_state = READ; 
 
 	std::set<uint8_t> write_set;
 	std::set<uint8_t> read_set;
+	hstd::shared_ptr<timed_insn_t> retire_insn;
 
 };
 
