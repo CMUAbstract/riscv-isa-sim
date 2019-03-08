f16268eb86d04e61298fcebb3b57df8986eb0260
g-ram <ggobieski@yahoo.com>
2019-03-08 17:09:40 -0500

    fixes to vmv

--- riscv/insns/vmv_v.h
+++ riscv/insns/vmv_v.h
@@ -1,3 +1,3 @@
 require_extension('V');
 for(uint8_t i = 0; i < VL; i++)
-	if(VS2P(i)) WRITEP_VRD(VS1P(i), i); else WRITEP_VRD(0, i);
+	if((VS2P(i) & 0x1) == 0x1) WRITEP_VRD(VS1P(i), i); else WRITEP_VRD(0, i);
