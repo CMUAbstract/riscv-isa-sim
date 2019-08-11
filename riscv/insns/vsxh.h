3c8fb743e9e7efcd7ff4494207d21ec1b10cf669
g-ram <ggobieski@thule.andrew.cmu.edu>
2019-08-11 12:28:40 -0400

    fixed vsxh

--- riscv/insns/vsxh.h
+++ riscv/insns/vsxh.h
@@ -1,5 +1,5 @@
 require_extension('V');
 for(uint16_t i = 0; i < VL; i++) {
 	if((insn.vm() && VMP(i)) || !insn.vm())
-		MMU.store_uint16(RS1 + sizeof(int16_t) * VS3P(i), VS2P(i));
+		MMU.store_uint16(RS1 + sizeof(int16_t) * VS2P(i), VS3P(i));
 }
